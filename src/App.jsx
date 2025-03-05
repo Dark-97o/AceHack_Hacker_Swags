aced0e69934878e5bd0a2236c5a5d70d78d6344c
Subhayan009kapas <subhayankapas47@gmail.com>
2025-03-05 08:36:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+import Swag from './Swags'
+
+function App() {
+
+
+  return (
+    <>
+     <Swag/>
+     
+    </>
+  )
+}
+
+export default App
